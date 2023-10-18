a9df1ddebfed9c5db8e61f79b01cf73d5284b258
Kai <kmichaelmikhail@gmail.com>
2023-10-18 09:53:27 +0200

    Set up base template

--- kaberg_design/settings.py
+++ kaberg_design/settings.py
@@ -25,6 +25,7 @@ runserver.default_port = "8001"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -97,7 +98,7 @@ ROOT_URLCONF = 'kaberg_design.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
