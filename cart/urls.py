59c820fe508e3b51d7f556fa993629643c717563
Kai <kmichaelmikhail@gmail.com>
2023-10-25 14:47:54 +0200

    Get CRUD functionality working for cart

--- cart/urls.py
+++ cart/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.view_cart, name='view_cart'),
     path('add/<item_id>/', views.add_to_cart, name='add_to_cart'),
+    path('update/<item_id>/', views.change_cart_quantity, name='change_cart_quantity'),
+    path('remove/<item_id>/', views.remove_from_cart, name='remove_from_cart'),
 ]
--- products/models.py
+++ products/models.py
@@ -34,7 +34,6 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     slug = models.SlugField(max_length=200, null=True, unique=True)
     description = models.TextField()
-    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     stock = models.IntegerField(blank=False, null=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
