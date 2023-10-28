67e3f6c21f4e0448d0a42cdd5d8b1e1cf76268e4
Kai <kmichaelmikhail@gmail.com>
2023-10-28 13:13:36 +0200

    Fix indenting issue resulting in data not saving

--- checkout/views.py
+++ checkout/views.py
@@ -147,17 +147,17 @@ def checkout_success(request, order_number):
         order.save()
 
         # Save the user's info
-    if save_info:
-        profile_data = {
-            'default_phone_number': order.phone_number,
-            'default_country': order.country,
-            'default_postcode': order.postcode,
-            'default_town_or_city': order.town_or_city,
-            'default_street_address1': order.street_address1,
-        }
-        user_profile_form = CustomerProfileForm(profile_data, instance=profile)
-        if user_profile_form.is_valid():
-            user_profile_form.save()
+        if save_info:
+            profile_data = {
+                'default_phone_number': order.phone_number,
+                'default_country': order.country,
+                'default_postcode': order.postcode,
+                'default_town_or_city': order.town_or_city,
+                'default_street_address1': order.street_address1,
+            }
+            user_profile_form = CustomerProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
 
     messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
