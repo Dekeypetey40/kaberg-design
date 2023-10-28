d616f5d889afcbd9d5cea35d24419a3e3ce39cba
Kai <kmichaelmikhail@gmail.com>
2023-10-28 14:36:13 +0200

    Try to force images to smaller height

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 from .models import Order, OrderLineItem
 from products.models import Product
-# from profiles.models import UserProfile
+from profiles.models import CustomerProfile
 
 import json
 import time
@@ -60,17 +60,17 @@ class StripeWH_Handler:
                 shipping_details.address[field] = None
 
         # Update profile information if save_info was checked
-        # profile = None
-        # username = intent.metadata.username
-        # if username != 'AnonymousUser':
-        #     profile = UserProfile.objects.get(user__username=username)
-        #     if save_info:
-        #         profile.default_phone_number = shipping_details.phone
-        #         profile.default_country = shipping_details.address.country
-        #         profile.default_postcode = shipping_details.address.postal_code
-        #         profile.default_town_or_city = shipping_details.address.city
-        #         profile.default_street_address1 = shipping_details.address.line1
-        #         profile.save()
+        profile = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            profile = CustomerProfile.objects.get(user__username=username)
+            if save_info:
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_street_address1 = shipping_details.address.line1
+                profile.save()
 
         order_exists = False
         attempt = 1
@@ -103,7 +103,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
-                    # user_profile=profile,
+                    customer_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
