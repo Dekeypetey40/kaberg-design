ab5790f01252ae9aef813af6891b1741aab599c7
Kai <kmichaelmikhail@gmail.com>
2023-10-28 13:06:37 +0200

    Remove unnecessary code

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -74,10 +74,8 @@ form.addEventListener('submit', function(ev) {
                     email: $.trim(form.email.value),
                     address:{
                         line1: $.trim(form.street_address1.value),
-                        line2: $.trim(form.street_address2.value),
                         city: $.trim(form.town_or_city.value),
                         country: $.trim(form.country.value),
-                        state: $.trim(form.county.value),
                     }
                 }
             },
