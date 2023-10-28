136a5ff9aef119ee53cd4350ed324f804843b23d
Kai <kmichaelmikhail@gmail.com>
2023-10-28 16:31:01 +0200

    Set up product management for admins

--- products/urls.py
+++ products/urls.py
@@ -6,6 +6,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.all_products_view, name='products'),
     path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
 ]
 
 if settings.DEBUG:
