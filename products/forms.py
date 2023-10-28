fc8ffe717ebc573bdb0151aff0b4bba79e33355d
Kai <kmichaelmikhail@gmail.com>
2023-10-28 15:18:03 +0200

    Set up CRUD for product owner

--- products/forms.py
+++ products/forms.py
@@ -1,7 +1,26 @@
 from django import forms
-
+from .widgets import CustomClearableFileInput
+from .models import Product, Category
 
 class ContactUsForm(forms.Form):
     name = forms.CharField(required=True)
     email = forms.EmailField(required=True)
     message = forms.CharField(required=True, widget=forms.Textarea)
+
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        names = [(c.id, c.get_name()) for c in categories]
+
+        self.fields['category'].choices = names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
