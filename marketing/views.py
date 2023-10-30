148ae5424326270e77235b4d9b2d9dfd8b64ede6
Kai <kmichaelmikhail@gmail.com>
2023-10-31 00:10:46 +0100

    Remove commented out code

--- marketing/views.py
+++ marketing/views.py
@@ -1,30 +1,16 @@
 from django.shortcuts import render
 
-from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 from django.core.validators import validate_email
-from django.contrib.auth.decorators import login_required
 
 from .models import SubscribedUsers
 
-import logging
-import hashlib
 
 from kaberg_design import settings
 from marketing.forms import SubscribeForm
-
-from mailchimp_marketing import Client
-from mailchimp_marketing.api_client import ApiClientError
-
-# logger = logging.getLogger(__name__)
-
-# mailchimp = Client()
-# api_key = settings.MAILCHIMP_API_KEY
-# server = settings.MAILCHIMP_DATA_CENTER
-# list_id = settings.MAILCHIMP_AUDIENCE_ID
 
 
 def subscribe(request):
